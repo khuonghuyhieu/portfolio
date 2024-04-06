0d0a99ccde25c1f35e07fb299a1b6d530cfd7a50
Hieu Khuong <khuonghuyhieu0705@gmail.com>
2024-04-06 22:32:59 +0700

    test SEO

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
 import { HomeView } from '@/views/Home'
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    openGraph: {
+      images: ['/logo_portfolio.png', ...previousImages],
+    },
+  }
+}
+
 export default function Home() {
   return <HomeView />
 }
